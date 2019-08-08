68558eda457b50d594d1964a7919cb24275e5e25
jahnvi01 <jbdalwadi01@gmail.com>
2019-08-08 20:42:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import Login from './components/login';
+import Home from './components/home';
+import Profile from './components/profile';
+
+
+const config = {
+  issuer: 'https://dev-545576.okta.com/oauth2/default',
+redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oa12z6ft6JjzgoDA357'
 }
+
+class App extends Component {
+  onAuthRequired=()=> {
+  // history.push('/login')
+  }
+  render() {
+  
+       
+     return (
+         <div className="App">
+
+<Router>
+         
+             <Security issuer={config.issuer}
+                   client_id={config.client_id}
+                   redirect_uri={config.redirect_uri}
+                   onAuthRequired={this.onAuthRequired()}
+         >
+          
+             <Route path='/' exact={true} component={Home} />
+           <SecureRoute path='/profile' exact={true} component={Profile} />
+           {/* <SecureRoute path='/protected' component={Protected} /> */}
+           <Route path='/login' render={() => <Login baseUrl='https://dev-545576.okta.com' />} />
+           <Route path='/implicit/callback' component={ImplicitCallback} />
+        
+        
+         </Security>
+          
+         </Router>
+           
+         </div>
+     )
+   }
+   }
+   
 
 export default App;
---
+++ src/components/profile.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class Profile extends Component {
+    render() {
+  
+    
+      return (
+            <div className="container">
+           <div className="profile-data row">
+         
+              
+              <div className="profile">
+           
+              <h2>Welcom To your profile</h2>
+           
+  </div>
+</div>
+          </div>
+        )
+    }
+}
+export default Profile;
